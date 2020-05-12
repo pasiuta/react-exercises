c471d7facebfdd5a54630548681ff9ad6c5c67e7
Dima <pasuta.dima@gmail.com>
2020-05-12 21:16:02 +0300

    add email forms

--- monster-lessons/src/Header.js
+++ monster-lessons/src/Header.js
@@ -1,5 +1,22 @@
-import React,{Component} from "react";
+import React,{Component,PropTypes} from "react";
 class  Header extends Component {
+    static propTypes = {
+    items: PropTypes.array.isRequired,
+        isLoading: PropTypes.bool,
+        submit: PropTypes.func.isRequired,
+        title: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(['news','photos']),
+        user: PropTypes.shape({
+            name: PropTypes.string,
+            age: PropTypes.number
+        }),
+        users: PropTypes.arrayOf(
+            PropTypes.shape({
+                name:PropTypes.string,
+                age:PropTypes.number
+            })
+        )
+    };
     render(){
         console.log('items',this.props.items)
         return (
