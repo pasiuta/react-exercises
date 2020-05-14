0eadf11b92af5f75544a06a9ef4c5b7b84d0ee6a
Dima <pasuta.dima@gmail.com>
2020-05-14 19:44:42 +0300

    add css style

---
+++ monster-lessons/build/precache-manifest.b57d3e35ef22d7af60e53d5a789752d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5c130e83f9ad2b9252c8506f1b70f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7de9ede118f29a4882e",
+    "url": "/static/css/main.e605e2dd.chunk.css"
+  },
+  {
+    "revision": "58599c4127338d15b043",
+    "url": "/static/js/2.c4ce9dcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4ce9dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7de9ede118f29a4882e",
+    "url": "/static/js/main.1ba7d6c0.chunk.js"
+  },
+  {
+    "revision": "46860da15321dddf99e8",
+    "url": "/static/js/runtime-main.828dd5b2.js"
+  }
+]);
--- monster-lessons/src/RegistrationForm.js
+++ monster-lessons/src/RegistrationForm.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-
+import  './RegistrationForm.css'
 class  RegistrationForm extends Component {
     constructor(props) {
         super(props);
@@ -26,8 +26,9 @@ class  RegistrationForm extends Component {
                placeholder="E-mail"
                value={this.state.email}
                onChange={this.handleEmailChange}
+               className="emailField"
                />
-               <button>Save</button>
+               <button className="submitBtn">Save</button>
            </form>
         );
     }
